8a03bff1b19fd22de374d060a5735a308aefa133
Aditi Chaudhry <aditichaudhry@Aditis-MBP.lan1>
2019-04-28 18:43:37 -0400

    helmet code

---
+++ helmet/helmet.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var https = require('https');
+var fs = require('fs');
+var helmet = require('helmet');
+
+var app = express();
+
+app.use(helmet())
+
+var options = {
+    key: fs.readFileSync('../aditiprivatekey.pem'),
+    cert: fs.readFileSync('../cert.pem')
+}
+
+https.createServer(options, app).listen(8000, function(err){
+    if (err) throw err;
+    console.log('Server started on port 8000');
+});
+
+app.get('/', function (req, res) {
+    res.header('Content-type', 'text/html');
+    return res.end('<h1>Securely served over HTTPS!!</h1>');
+});
